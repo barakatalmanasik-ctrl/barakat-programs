5f27f31bed67bf32a6293cc27f525cbb165a560c
barakatalmanasik-ctrl <barakatalmanasik@gmail.com>
2026-08-26 20:04:54 +0300

    config: set correct Supabase anon key

--- js/services/supabase-config.js
+++ js/services/supabase-config.js
@@ -2,5 +2,5 @@
 
 const SUPABASE_CONFIG = {
   url: 'https://fulgfalcqerwcvfbahel.supabase.co',
-  anonKey: 'sb_publishable_AUB0Bq5JDH_aoIEk0nV6kg_T710Calx'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1bGdmYWxjcWVyd2N2ZmJhaGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NTc0NjEsImV4cCI6MjEwMzMzMzQ2MX0.f7cWAa_QUiLFoqUHCjx4Hkv9mTRaS6-ZOqg4b1-PeNQ'
 };
